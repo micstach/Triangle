6e1fa0aa40827f25c5a712dc3d1d01b87feb2328
Michał Stachańczyk <Micha Stachaczyk>
2016-01-23 19:47:32 +0100

    files added

---
+++ Triangle/Triangle.cpp
@@ -0,0 +1,80 @@
+//#include <stdafx.h>
+
+#include <GLFW/glfw3.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+float speed = 0.0f;
+
+static void error_callback(int error, const char* description)
+{
+	fputs(description, stderr);
+}
+
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+	if (key == GLFW_KEY_SPACE)
+		speed = 0.f;
+	else if (key == GLFW_KEY_UP)
+		speed += 1.0f;
+	else if (key == GLFW_KEY_DOWN)
+		speed -= 1.0f;
+	else if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, GL_TRUE);
+}
+
+int WinMain(void)
+{
+	GLFWwindow* window;
+	glfwSetErrorCallback(error_callback);
+	if (!glfwInit())
+		exit(EXIT_FAILURE);
+	window = glfwCreateWindow(640, 480, "Simple example", NULL, NULL);
+	if (!window)
+	{
+		glfwTerminate();
+		exit(EXIT_FAILURE);
+	}
+	glfwMakeContextCurrent(window);
+	glfwSwapInterval(1);
+	glfwSetKeyCallback(window, key_callback);
+
+	float time = (float)glfwGetTime();
+	float deltaTime = 0.0f;
+	float angle = 0.0f;
+
+	while (!glfwWindowShouldClose(window))
+	{
+		float ratio;
+		int width, height;
+		glfwGetFramebufferSize(window, &width, &height);
+		ratio = width / (float)height;
+		glViewport(0, 0, width, height);
+		glClear(GL_COLOR_BUFFER_BIT);
+		glMatrixMode(GL_PROJECTION);
+		glLoadIdentity();
+		glOrtho(-ratio, ratio, -1.f, 1.f, 1.f, -1.f);
+		glMatrixMode(GL_MODELVIEW);
+		glLoadIdentity();
+		glRotatef( angle, 0.f, 0.f, 1.f);
+		glBegin(GL_TRIANGLES);
+		glColor3f(1.f, 0.f, 0.f);
+		glVertex3f(-0.6f, -0.4f, 0.f);
+		glColor3f(0.f, 1.f, 0.f);
+		glVertex3f(0.6f, -0.4f, 0.f);
+		glColor3f(0.f, 0.f, 1.f);
+		glVertex3f(0.f, 0.6f, 0.f);
+		glEnd();
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+
+	    deltaTime = (float)glfwGetTime() - time;
+
+		angle += speed * deltaTime;
+
+		time = (float)glfwGetTime();
+	}
+	glfwDestroyWindow(window);
+	glfwTerminate();
+	exit(EXIT_SUCCESS);
+}
